d4e84605705d5654c1767e614ae498bf00c76263
Sam Rahim <19746759+samjakbal@users.noreply.github.com>
2023-04-21 00:51:13 +0200

    Fixed tcpdump command not found.

--- activate-toon.sh
+++ activate-toon.sh
@@ -22,8 +22,8 @@ read QUESTION
 echo "Blocking all HTTPS (and therefore Toon VPN). Now we wait until the Toon disconnects the VPN and sends traffic towards the service center on the wifi. After a minute or so start the activation (use a random activation code and retry until it succeeds. Don't go back to the home activation screen."
 /sbin/iptables -I FORWARD -p tcp --dport 443 -j DROP
 
-OUTPUT=`/usr/sbin/tcpdump -n -i any port 31080 -c 1 2>/dev/null` || exit "tcpdump failed"
-IP=`echo $OUTPUT | cut -d\  -f5 | cut -d\. -f1,2,3,4`
+OUTPUT=`tcpdump -n -i any port 31080 -c 1 2>/dev/null` || exit "tcpdump failed"
+IP=`echo $OUTPUT | cut -d\  -f7 | cut -d\. -f1,2,3,4`
 
 
 echo "The Toon is connecting to IP: $IP"
